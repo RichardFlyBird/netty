17ce4580970642d09a4d4adf74b9d9a30f1233ac
richard.ji <1148705834@qq.com>
2025-04-26 11:23:23 +0800

    编译common包，启动HttpHelloWorldServer Demo

--- transport/src/main/java/io/netty/channel/AbstractChannel.java
+++ transport/src/main/java/io/netty/channel/AbstractChannel.java
@@ -575,7 +575,7 @@ public abstract class AbstractChannel extends DefaultAttributeMap implements Cha
                 invokeLater(new Runnable() {
                     @Override
                     public void run() {
-                        pipeline.fireChannelActive();
+                        pipeline.fireChannelActive(); // 这里
                     }
                 });
             }
